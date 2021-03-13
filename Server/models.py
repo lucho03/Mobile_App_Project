bbcbed13c2cdece2466ab65412201adfea84d4ca
Gandalf The Blue <sami_dimi@abv.bg>
2021-03-13 20:15:04 +0200

    added requirments

--- Server/models.py
+++ Server/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime, Enum
 from sqlalchemy.orm import relationship
 from database import Base
 
